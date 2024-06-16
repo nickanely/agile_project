06851f94b234aced416d2a4752e63c7d1929b383
Nikoloz aneli <anelinikolozi@gmail.com>
2024-06-17 02:01:25 +0400

    fixed commented issues

--- dags/MSI_7/skip_messages_on_holiday_dag.py
+++ dags/MSI_7/skip_messages_on_holiday_dag.py
@@ -3,15 +3,15 @@ from airflow import DAG
 from airflow.operators.empty import EmptyOperator
 from airflow.operators.python import PythonOperator, BranchPythonOperator
 
-from MSI_7.check_skip_date import check_skip_date
-from MSI_7.image_loader import load_image
-from MSI_7.quote_loader import load_quote
-from MSI_7.teams_notifier import send_to_teams
+from MSI_7.scripts.check_skip_date import check_skip_date
+from MSI_7.scripts.image_loader import load_image
+from MSI_7.scripts.quote_loader import load_quote
+from MSI_7.scripts.teams_notifier import send_to_teams
 
 with DAG(
-        dag_id="MSI_7_skip_holidays_dag",
+        dag_id="skip_messages_on_holiday_dag",
         description="A simple DAG to send daily quote with image to Teams - skip holidays",
-        start_date=pendulum.datetime(2023, 10, 16),
+        start_date=pendulum.datetime(2023, 10, 17),
         schedule_interval="@daily",
         catchup=False,
 ) as dag:
@@ -21,25 +21,21 @@ with DAG(
 
     load_quote_task = PythonOperator(
         task_id="load_quote",
-        provide_context=True,
         python_callable=load_quote,
     )
 
     load_image_task = PythonOperator(
         task_id="load_image",
-        provide_context=True,
         python_callable=load_image,
     )
 
     check_skip_date_task = BranchPythonOperator(
         task_id="check_skip_date",
-        provide_context=True,
         python_callable=check_skip_date,
     )
 
     send_to_teams_task = PythonOperator(
         task_id="send_to_teams",
-        provide_context=True,
         python_callable=send_to_teams,
     )
     skip_message = EmptyOperator(
